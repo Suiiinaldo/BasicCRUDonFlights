ce23bc99f136a121d624429050627bf1f1c85f8a
Suiiinaldo <toprakhar03@gmail.com>
2024-01-06 03:40:28 +0530

    Added get api

--- src/controller/airplane-controller.js
+++ src/controller/airplane-controller.js
@@ -2,6 +2,11 @@ const { AirplaneService } = require("../services");
 const { StatusCodes} = require("http-status-codes");
 const { SuccessResponse, ErrorResponse} = require("../utils/common")
 
+
+/*
+ * POST : /airplanes 
+ * req-body : {modelNumber: 'airbusA380' , capacity : 200 }
+ */
 async function createAirplane(req,res){
     try {
         const airplane = await AirplaneService.createAirplane({
@@ -20,6 +25,10 @@ async function createAirplane(req,res){
     }
 }
 
+/*
+ * GET : /airplanes
+ * req-body : {}
+ */
 async function getAirplanes(req,res){
     try {
         const airplanes = await AirplaneService.getAirplanes();
@@ -35,8 +44,27 @@ async function getAirplanes(req,res){
     }
 }
 
+/*
+ * GET : /airplanes/:id
+ * req-body : {}
+ */
+async function getAirplane(req,res){
+    try {
+        const airplanes = await AirplaneService.getAirplane(req.params.id);
+        SuccessResponse.data = airplanes;
+        return res
+                .status(StatusCodes.OK)
+                .json(SuccessResponse); 
+    } catch (error) {
+        ErrorResponse.error = error;
+        return res
+                .status(error.statusCodes)
+                .json(ErrorResponse);
+    }
+}
+
 module.exports = {
     createAirplane,
     getAirplanes,
-    
+    getAirplane,
 }
--- src/services/airplane-service.js
+++ src/services/airplane-service.js
@@ -25,11 +25,24 @@ async function getAirplanes(){
         const airplanes = await airplaneRepository.getAll();
         return airplanes;
     } catch (error) {
-        throw new AppError('Cannot fetch data of all the  airplanes',StatusCodes.INTERNAL_SERVER_ERROR);
+        throw new AppError('Cannot fetch data of all the airplanes',StatusCodes.INTERNAL_SERVER_ERROR);
+    }
+}
+
+async function getAirplane(id){
+    try {
+        const airplane = await airplaneRepository.get(id);
+        return airplane;
+    } catch (error) {
+        if(error.statusCodes == StatusCodes.NOT_FOUND){
+            throw new AppError(" The airplane you requested is not present ", error.statusCodes);
+        }
+        throw new AppError('Cannot fetch data of the airplanes',StatusCodes.INTERNAL_SERVER_ERROR);
     }
 }
 
 module.exports = {
     createAirplane,
     getAirplanes,
+    getAirplane,
 }
