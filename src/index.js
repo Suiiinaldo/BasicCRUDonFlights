74eb04e409afdb4fc9eb66e9ef654ee201c04ac9
Suiiinaldo <toprakhar03@gmail.com>
2024-01-09 20:03:17 +0530

    removed async property from app.listen callback

--- src/index.js
+++ src/index.js
@@ -11,20 +11,7 @@ app.use(express.urlencoded({extended: true}));
 
 app.use("/api", apiRoutes);
 
-app.listen(ServerConfig.PORT, async () => {
+app.listen(ServerConfig.PORT, () => {
   console.log(`Successfully started the server on PORT : ${ServerConfig.PORT}`);
   Logger.info("Successfully started the server", "root", { msg: "something" });
-  //Testing Purpose
-  // const { City, Airport } = require('./models');
-  // // const newDelhi = await City.findByPk(3);
-  // // console.log(newDelhi);
-  // // const airport = await newDelhi.createAirport({name : 'Delhi airport',code : 'NDLS'});
-  // // console.log(airport);
-  // // const indira = await Airport.findByPk(1);
-  // // await newDelhi.removeAirports(indira);
-  // await City.destroy({
-  //   where:{
-  //     id:3
-  //   }
-  // });
 });
