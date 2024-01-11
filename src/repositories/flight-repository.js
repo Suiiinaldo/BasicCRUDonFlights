e30af10d93e5216a008558af2926ef02ed3a5d0f
Suiiinaldo <toprakhar03@gmail.com>
2024-01-12 03:12:03 +0530

    Added complex join for fetching all flights with airports and airplane details with now city details also

--- src/repositories/flight-repository.js
+++ src/repositories/flight-repository.js
@@ -1,5 +1,5 @@
 const CrudRepository = require("./crud-repository");
-const { Flight, Airplane, Airport } = require("../models");
+const { Flight, Airplane, Airport, City } = require("../models");
 const { Sequelize } = require("sequelize");
 
 class FlightRepository extends CrudRepository {
@@ -23,7 +23,11 @@ class FlightRepository extends CrudRepository {
                     required: true,
                     on : {
                         col1: Sequelize.where(Sequelize.col("Flight.departureAirportId"), "=", Sequelize.col("departureAirport.code")),
-                    }
+                    },
+                    include : {
+                        model: City,
+                        required: true,
+                    },
                 },
                 {
                     model: Airport,
@@ -31,7 +35,11 @@ class FlightRepository extends CrudRepository {
                     required: true,
                     on : {
                         col1: Sequelize.where(Sequelize.col("Flight.arrivalAirportId"), "=", Sequelize.col("arrivalAirport.code")),
-                    }
+                    },
+                    include : {
+                        model: City,
+                        required: true,
+                    },
                 },
 
             ]
