fd044097d6b010bf654ca000df940d53a60ed7f4
Suiiinaldo <toprakhar03@gmail.com>
2024-01-24 00:55:43 +0530

    Removed Logger from main index file

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 
-const { ServerConfig, Logger } = require("./config");
+const { ServerConfig } = require("./config");
 
 const apiRoutes = require("./routes");
 
@@ -13,5 +13,5 @@ app.use("/api", apiRoutes);
 
 app.listen(ServerConfig.PORT, () => {
   console.log(`Successfully started the server on PORT : ${ServerConfig.PORT}`);
-  Logger.info("Successfully started the server", "root", { msg: "something" });
+  // Logger.info("Successfully started the server", "root", { msg: "something" });
 });
